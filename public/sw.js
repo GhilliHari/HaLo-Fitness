ec87a75db871b86661b81b807a0b86f0ee974e3a
Hari <hari.doss7@gmail.com>
2026-09-21 19:57:59 +0530

    Remove download .apk mentions from sidebar and header

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,16 @@
-const CACHE_NAME = 'halo-fitness-v19';
+const CACHE_NAME = 'halo-fitness-v20';
 const ASSETS = [
   '/',
   '/index.html',
   '/css/style.css',
-  '/js/app.js?v=19',
-  '/js/data.js?v=19',
-  '/js/translations.js?v=19',
-  '/js/muscleMap.js?v=19',
-  '/js/foodParser.js?v=19',
-  '/js/tracker.js?v=19',
-  '/js/pedometer.js?v=19',
-  '/js/connectedServices.js?v=19'
+  '/js/app.js?v=20',
+  '/js/data.js?v=20',
+  '/js/translations.js?v=20',
+  '/js/muscleMap.js?v=20',
+  '/js/foodParser.js?v=20',
+  '/js/tracker.js?v=20',
+  '/js/pedometer.js?v=20',
+  '/js/connectedServices.js?v=20'
 ];
 
 self.addEventListener('install', e => {
